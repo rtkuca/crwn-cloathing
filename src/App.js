16a675787dc93a336277903d06d16a7da7778a07
rtkuca <rtkuca@hotmail.com>
2021-09-30 20:44:36 -0300

    funcionalidades do checkout e cartitem adicionadas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from "./pages/signin-signup/signin-signup.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from "./pages/signin-signup/signin-signup.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 
 class App extends React.Component
@@ -52,6 +56,7 @@ class App extends React.Component
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
+                    <Route path='/checkout' component={CheckoutPage} />
                     <Route exact
                            path='/signin'
                            render={() =>
@@ -64,8 +69,8 @@ class App extends React.Component
     }
 }
 
-const mapStateToProps = ({ user }) => ({
-   currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+   currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
